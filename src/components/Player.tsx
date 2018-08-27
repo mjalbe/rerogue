a0e9fba9846cb023c1f4e5a6bf99e6f2a57c1f20
Jason Smith <jasmith@zillowgroup.com>
2018-08-27 10:09:32 -0700

    player ts

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import * as React from 'react';
 
 class Player extends React.Component {
-    render() {
+    public render() {
         return (
             <div className="char player t_0"/>
         );
